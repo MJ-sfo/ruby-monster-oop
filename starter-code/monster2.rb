a652124f7fcd6f07e6c17488a14ec2cd0e33b740
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-16 16:56:40 -0700

    started zombie class

---
+++ starter-code/monster2.rb
@@ -0,0 +1,42 @@
+class Monster2
+  attr_accessor :threat_level
+  def initialize(threat_level)
+  	@threat_level = threat_level
+  end
+
+  def get_dangerous
+    # ruby version of a switch statement is case
+    case @threat_level
+      when :low
+        @threat_level = :medium
+      when :medium
+        @threat_level = :high
+      when :high
+        @threat_level = :midnight
+      when :midnight
+        :midnight
+    end
+  end
+end
+
+class Werewolf < Monster2
+end
+
+class  Zombie < Monster2
+  attr_accessor :habitat, :name
+
+  def initialize
+    @habitat = "graveyard"
+    @name
+  end
+end
+
+
+rob = Zombie.new
+puts rob.habitat
+# "graveyard"
+puts rob.threat_level
+# :medium
+rob.name = "Rob Zombie"
+puts rob.name
+# "Rob Zombie"
