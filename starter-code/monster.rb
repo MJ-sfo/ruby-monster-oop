10261118ecace29fd52c412013e37824e0298bed
MJ-sfo <michaeljlaird@yahoo.com>
2017-05-16 16:29:05 -0700

    finished first part

--- starter-code/monster.rb
+++ starter-code/monster.rb
@@ -1,7 +1,37 @@
 ### monster.rb - run this file
 
 # Monster class
+class Monster
+  @@count = 0
+  attr_accessor :habitat, :threat_level
 
+  def initialize(threat_level = :medium)
+    p "Rawr!"
+    @habitat = habitat
+    @threat_level = threat_level
+    if !([:low, :medium, :high, :midnight].include? @threat_level)
+      p "unacceptable treath level !!"
+      exception
+    else
+      p "threat level is expected"
+    end
+    @@count +=1
+    p "#{@@count} monsters now roam the world!"
+  end
+
+  def habitat? (test_habitat)
+    test_habitat == @habitat
+  end
+
+  def self.count
+    @@count
+  end
+
+  def exception
+    p "now in exception method"
+    raise RuntimeError.new("cannot create monster - invalid threat level: #{@threat_level} ")
+  end
+end
 
 
 # Zombie class
@@ -10,11 +40,11 @@
 
 # Werewolf class
 
- 
+
 
 # Flying module
 
-  
+
 
 # Vampire class
 
@@ -22,4 +52,31 @@
 
 
 ### "Driver" Code Area
+# m = Monster.new
+# p m
+#
+# rabbit = Monster.new
+# rabbit.habitat = "Cave of Caerbannog"
+# p rabbit.habitat
 
+dalek = Monster.new(:high)
+p dalek.threat_level
+
+teletubby = Monster.new
+p teletubby.threat_level
+
+# Challenge 6
+yeti = Monster.new
+yeti.habitat = "tundra"
+p yeti.habitat?("swamp")
+p yeti.habitat?("tundra")
+
+# Challenge 7
+predator = Monster.new(:high)
+alien = Monster.new(:high)
+
+# Challenge 8
+p Monster.count
+
+# Challenge 9
+p rubber_ducky = Monster.new(:friendly)
